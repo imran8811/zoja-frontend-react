711d92a8b681a4b47379f8dc7fb1e24c9404aaa8
Mohd Imran <m.imran8811@gmail.com>
2022-05-22 08:49:49 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -5,5 +5,8 @@ module.exports = {
   trailingSlash : true,
   sassOptions: {
     includePaths: [path.join(process.cwd(), 'src', 'scss')],
+  },
+  env:{
+    GOOGLE_ANALYTICS:process.env.GOOGLE_ANALYTICS,
   }
 }
