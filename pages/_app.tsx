aa267347ee446d0ec69dad5c96774d9a63f98f06
Mohd Imran <m.imran8811@gmail.com>
2023-02-10 19:18:07 +0500

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import store from '../src/store';
 import { Provider } from 'react-redux';
 import Script from 'next/script';
+import GoogleAnalytis from '../src/components/ga';
 
 import 'swiper/swiper.min.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -13,6 +14,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Component {...pageProps} />
+      <GoogleAnalytis />
     </>
   ) 
 }
